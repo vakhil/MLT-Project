d42aa9da5b3b31606468053888a5524108bf55d5
Akhil Vemula <akhilvemula95@gmail.com>
2016-04-11 11:22:57 +0530

    Checking Vibhor's rectangles

--- bf1.py
+++ bf1.py
@@ -44,9 +44,9 @@ clf = joblib.load('shane.pkl')
 
 print type(clf)
 
-cap = cv2.VideoCapture('../../../videoData/datasample1.mov')
+cap = cv2.VideoCapture('../../../videoData/input_video_sample1.mov')
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-fgbg = cv2.BackgroundSubtractorMOG2(history=500,varThreshold=10, bShadowDetection= False)
+fgbg = cv2.BackgroundSubtractorMOG(history=500,varThreshold=20, bShadowDetection= False)
 k=0
 
 label=0
@@ -57,28 +57,21 @@ while(1):
 	height, width = frame.shape[:2]
 	frame = cv2.resize(frame,(900, 700), interpolation = cv2.INTER_CUBIC)
 	fgmask = fgbg.apply(frame)
-	#fgmask = fgbg.apply(frame,learningRate=0.5)
-	#fg1 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+	
 	fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 	
 	fgamsk= cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
 	fgmask = cv2.GaussianBlur(fgmask, (21, 21), 0)
-	#fgmask = cv2.dilate(fgmask,kernel,iterations = 1)
+	
 	cnts, hierarchy = (cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) ) 
-	#_,contours,hierarchy = cv2.findContours(fgmask.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-	print len(cnts)
-	#print type(k[2])
 	
 	
-	#u = k[2]
-#i,cnts,u = (cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) ) 
-	
-	
+
 	mg2_fg = cv2.bitwise_and(frame,frame,mask = fgmask)
 	for c in cnts:
 
 		# if the contour is too small, ignore it
-		if cv2.contourArea(c) < 8000:
+		if cv2.contourArea(c) < 10000:
 			continue
 		
 		(x, y, w, h) = cv2.boundingRect(c)
@@ -90,52 +83,17 @@ while(1):
 	  
 		cv2.imwrite('./data/1img{:>07}.jpg'.format(k),im)
 		
-		col,row=im.shape
-		Y=np.zeros((col,row),dtype=np.uint8)
-		for a in range(0,col):
-			for b in range(0,row):
-				Y[a][b]=im[a,b]
-		Z1=hog.compute(Y,winStride,padding,locations)#
-		lists = []
-		finale = []
-		for i in range(0, len(Z1)) :	
-				lists.append( Z1[i][0])
-		finale.append(lists)
-
-		print Z1.shape
-		k=k+1 # Dumps your pc :P
-		Z12=np.zeros(50000,dtype=np.float64)
-		for u in range(0,Z1.size):
-			Z12[u]=Z1[u][0]
-	  	finale = array(finale)
-		Z12=Z12.reshape(1,-1)
-		start_time = timeit.default_timer()
-		label=clf.predict(finale)
-		elapsed = timeit.default_timer() - start_time
-		print elapsed
-		k=k+1
-		# if label[0]==1:
-		# 	cv2.putText(frame,"Autorikshaw",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),thickness=2,lineType=8,bottomLeftOrigin=False)
-		if label[0]==3:
-			cv2.putText(frame,"Car",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),thickness=2,lineType=8,bottomLeftOrigin=False)
-		elif label[0]==1:
-			cv2.putText(frame,"Cycle",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),thickness=2,lineType=8,bottomLeftOrigin=False)
-		elif label[0]==2:
-			cv2.putText(frame,"Motorcycle",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),thickness=2,lineType=8,bottomLeftOrigin=False)
-		elif label[0]==4:
-			cv2.putText(frame,"Person",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),thickness=2,lineType=8,bottomLeftOrigin=False)
-		# elif label[0]==6:
-		# 	cv2.putText(frame,"Rikshaw",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),thickness=2,lineType=8,bottomLeftOrigin=False)
+		
 
 
 
    
 
 	cv2.imshow('frame',frame)
-	# ret, frame = cap.read()
-	# ret, frame = cap.read()
-	# ret, frame = cap.read()
-	# ret, frame = cap.read()
+	ret, frame = cap.read()
+	ret, frame = cap.read()
+	ret, frame = cap.read()
+	ret, frame = cap.read()
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
